f90f7971f9a556aa52b76dbcce43cd34c5a63704
programmerrafi <mdrafiprogrammer@gmail.com>
2023-01-21 03:47:03 +0600

    Video card component done

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ["portfolio-mdrafi.vercel.app"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
